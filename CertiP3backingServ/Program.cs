8697546d3884b5739d51dafec8e39e19745b80d0
Andrew Alvarez Jordan <andrewjordan1@upb.edu>
2025-05-12 01:43:29 -0400

    develop: Updated program

--- CertiP3backingServ/Program.cs
+++ CertiP3backingServ/Program.cs
@@ -26,11 +26,9 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 
     app.UseAuthorization();
 
